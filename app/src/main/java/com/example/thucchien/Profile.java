7bb501ce5273756c303878f7f2499802a3192523
thanghaqn2000 <57361234+thanghaqn2000@users.noreply.github.com>
2020-10-18 20:39:59 +0700

    first commit

---
+++ app/src/main/java/com/example/thucchien/Profile.java
@@ -0,0 +1,24 @@
+package com.example.thucchien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Profile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+    public void backlogin(View view) {
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+    }
+    public void ListView(View view) {
+        startActivity(new Intent(getApplicationContext(),ListView.class));
+    }
+    public void logout(View view) {
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+    }
+}
